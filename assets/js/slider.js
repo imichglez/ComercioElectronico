790a70f4b6a0074982fe18fcf69e3a6984e7b378
Ingrid González <glez.ingrid23@gmail.com>
2024-11-08 17:06:54 +0100

    nav bar

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -20,3 +20,28 @@ navLinks.forEach((link, index) => {
         slides[currentSlide].classList.add('active');
     });
 });
+
+// ---------- NAV BAR ---------- 
+
+const nav = document.querySelector(".nav"),
+  searchIcon = document.querySelector("#searchIcon"),
+  navOpenBtn = document.querySelector(".navOpenBtn"),
+  navCloseBtn = document.querySelector(".navCloseBtn");
+
+searchIcon.addEventListener("click", () => {
+  nav.classList.toggle("openSearch");
+  nav.classList.remove("openNav");
+  if (nav.classList.contains("openSearch")) {
+    return searchIcon.classList.replace("uil-search", "uil-times");
+  }
+  searchIcon.classList.replace("uil-times", "uil-search");
+});
+
+navOpenBtn.addEventListener("click", () => {
+  nav.classList.add("openNav");
+  nav.classList.remove("openSearch");
+  searchIcon.classList.replace("uil-times", "uil-search");
+});
+navCloseBtn.addEventListener("click", () => {
+  nav.classList.remove("openNav");
+});
